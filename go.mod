f4b7b965bbaa7524a58c3146c6f46f00753b983c
swordde <heavenlydemon2024@gmail.com>
2026-03-24 22:48:29 +0530

    the great  beginig of the idhanft here  i test the docker with postrgresss even though its not required hahahahahhahaha

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module idhanft
 
 go 1.25.0
+
+require github.com/lib/pq v1.12.0
--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Print("hello stupid world!!!!!")
+	db_url := "postgres://swordemon:@localhost:5432/uddhanthodu?sslmode=disable"
 
-	fmt.Print("hello stupid world!!!!!")
-	fmt.Print("hello stupid world!!!!!")
-	fmt.Print("hello stupid world!!!!!")
+	db, err := sql.Open("postgres", db_url)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
 }
