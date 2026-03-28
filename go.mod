d4779091cb0146fa7e9cb14730ad82d27fd35a68
swordde <heavenlydemon2024@gmail.com>
2026-03-28 20:11:46 +0530

    working adding file ,and reading cotent form the file!!!!haha hah:

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module idhanft
 
 go 1.25.0
-
-require github.com/lib/pq v1.12.0
--- main.go
+++ main.go
@@ -2,20 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
+type totdo struct{}
+
 func main() {
-	fmt.Print("enter the process:")
-	var process int
+	for true {
 
-	fmt.Scan(&process)
+		fmt.Println("enter the process u want to pick!!!")
+		i := 0
+		fmt.Scan(&i)
 
+		switch i {
+		case 1:
+			var name, discription string
+			fmt.Scan(&name)
+			fmt.Scan(&discription)
+			case1(name, discription)
 
-	switch i := fmt.Scan(){
-		
+		case 2:
+			case2()
 
+		case 3:
+			fmt.Println("case 2")
 
-	} 
+		case 4:
+			fmt.Println("case 2")
+
+		case 5:
+			fmt.Println("case 2")
+		}
+	}
 	fmt.Print("")
-	fmt.Println(process)
+}
+
+func case1(name, description string) {
+	_, err := os.Open("activities.txt") // For read access.
+	if err != nil {
+		log.Fatal(err)
+
+		fmt.Print("the errorr here")
+	}
+	fmt.Println("process is set", name, description)
+
+	bytes := []byte(description)
+	err = os.WriteFile("activities.txt", bytes, 0o666)
+	if err != nil {
+		fmt.Print("")
+		log.Fatal(err)
+	}
+}
+
+func case2() {
+	file, err := os.Open("activities.txt") // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(" all the tasks are \n  %q \n ", data[:count])
 }
