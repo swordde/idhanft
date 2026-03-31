429a890a38795185110016fc8dfaa44eb9a478a2
swordde <heavenlydemon2024@gmail.com>
2026-03-31 15:59:48 +0530

    some more importand and the time stamp is added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type totdo struct{}
@@ -11,7 +12,7 @@ type totdo struct{}
 func main() {
 	for {
 
-		fmt.Println("enter the process u want to pick!!!")
+		fmt.Println("\n enter the process u want to pick!!!")
 		i := 0
 		fmt.Print(i)
 		fmt.Scan(&i)
@@ -21,8 +22,10 @@ func main() {
 			var name, discription string
 			fmt.Scan(&name)
 			fmt.Scan(&discription)
-			stringcombiner6000(name, discription)
-			case1(name, discription)
+			tim := time.Now().Format("2006-01-02 15:04:05")
+			fmt.Printf("%T", tim)
+
+			case1(name, discription, tim)
 
 		case 2:
 			case2()
@@ -40,7 +43,8 @@ func main() {
 	fmt.Print("")
 }
 
-func case1(name, description string) {
+func case1(name, description, tim string) {
+	value := stringcombiner6000(name, description, tim)
 	filename := "activities.txt"
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 	if err != nil {
@@ -49,7 +53,7 @@ func case1(name, description string) {
 	}
 	defer f.Close()
 
-	if _, err := f.WriteString(description); err != nil {
+	if _, err := f.WriteString(value); err != nil {
 		fmt.Println("Error writing to file:", err)
 		return
 	}
@@ -69,8 +73,9 @@ func case2() {
 	fmt.Printf(" all the tasks are \n  %q \n ", data[:count])
 }
 
-func stringcombiner6000(name, descrition string) {
-	str := name + " " + descrition
+func stringcombiner6000(name, descrition, tim string) (str string) {
+	str = "\n name:" + name + " " + "desription" + descrition + "setdate:" + tim + "\n"
 
 	fmt.Println(str)
+	return str
 }
